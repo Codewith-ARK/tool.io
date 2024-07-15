6aa369b514c9d1d2b6f586a6ce3bea46bd19f6b1
Abdul Rehman Khan <khan456.tdm@gmail.com>
2024-07-15 13:10:33 +0500

    [🛠️]

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,27 @@
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", ({ matches }) => {
-    if (matches) {
-      document.querySelector("html").setAttribute("data-theme", "halloween");
-    } else {
-      document.querySelector("html").setAttribute("data-theme", "fantasy");
-    }
-  });
-
-document.querySelector("#showPassword").addEventListener("change", () => {
-  const elem = document.getElementById("password");
-  if (elem.type === "password") {
-    elem.type = "text";
+// Function to set the theme based on the user's color scheme preference
+function setThemeBasedOnPreference(prefersDarkScheme) {
+  if (prefersDarkScheme) {
+    document.querySelector("html").setAttribute("data-theme", "halloween");
   } else {
-    elem.type = "password";
+    document.querySelector("html").setAttribute("data-theme", "fantasy");
   }
+}
+
+// Set the initial theme based on the current preference
+setThemeBasedOnPreference(window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+// Add an event listener to respond to changes in the color scheme preference
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
+  setThemeBasedOnPreference(matches);
 });
+
+if(location.href === "/login" || location.href === "/signup"){
+  document.querySelector("#showPassword").addEventListener("change", () => {
+    const elem = document.getElementById("password");
+    if (elem.type === "password") {
+      elem.type = "text";
+    } else {
+      elem.type = "password";
+    }
+  });  
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     require('daisyui')
   ],
   daisyui: {
-    themes: ['fantasy', 'halloween'],
+    themes: ['halloween', 'fantasy'],
   }
 }
 
